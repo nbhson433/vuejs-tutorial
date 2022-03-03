6141fdcf5e086a5974a33dbe1d5fb78f52d5e2e0
Son Nguyen <nbhson43@gmail.com>
2022-03-03 20:37:44 +0700

    update props

---
+++ 2_Vue-cli/4_props_emit/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Parent Component: {{ name }}</div>
+  <button class="btn btn-sm btn-outline-success" @click="name = 'Phương Trinh'">
+    Change Name
+  </button>
+  <hr />
+  <hello-work-component :name="name" :age="20" @nameChange="name = $event"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      name: "Sơn Nguyễn",
+    };
+  },
+  components: {
+    "hello-work-component": HelloWorld,
+  },
+});
+</script>
---
+++ 2_Vue-cli/4_props_emit/src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <span class="hello">
+    {{ name }} - {{ age }}
+
+    <br />
+    {{ reverseName() }}
+    <button
+      class="btn btn-sm btn-outline-success"
+      @click="changeName()"
+    >
+      Change Name
+    </button>
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  data() {
+    return {
+      nameAssign: ''
+    }
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: "Default Name",
+    },
+    age: {
+      type: Number,
+    },
+  },
+  beforeUpdate() {
+    this.nameAssign = this.name
+  },
+  methods: {
+    reverseName() {
+      return this.name?.split("").reverse().join("");
+    },
+    changeName() {
+     this.nameAssign = 'Sơn Nguyễn'
+     this.$emit('nameChange', this.nameAssign)
+    }
+  },
+});
+</script>
