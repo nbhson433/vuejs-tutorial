7151fcc9350678f0e06a24bc5c2f4b1e159ef5fb
Son Nguyen <nbhson43@gmail.com>
2022-03-23 21:18:53 +0700

    provide inject

---
+++ 2_Vue-cli/9_Provide_Inject/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>
+     <input v-model="dangerMessage">
+    <ChildComponent/>
+  </p>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import ChildComponent from './components/Child-Component.vue'
+
+export default defineComponent({
+  name: 'App',
+  provide() {
+    return {
+      warnMsg: this.warnMessage,
+      dangerMsg: computed(() => this.dangerMessage),
+    }
+  },
+  data() {
+    return {
+      warnMessage: 'Warning message!',
+      dangerMessage: 'Danger message!'
+    };
+  },
+  components: {
+    ChildComponent
+  },
+});
+</script>
---
+++ 2_Vue-cli/9_Provide_Inject/src/components/Child-Component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{warnMsg}}
+  </div>
+  <div>
+    {{dangerMsg}}
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ChildComponent',
+  inject: ['warnMsg', 'dangerMsg'],
+  data() {
+    return {
+
+    }
+  },
+});
+</script>
