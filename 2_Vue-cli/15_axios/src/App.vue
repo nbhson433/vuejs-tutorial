7b8159d4dc93ac3166ad04131e574c50b3f6fb1b
Son Nguyen <nbhson43@gmail.com>
2022-03-30 14:34:50 +0700

    axios interceptor

---
+++ 2_Vue-cli/15_axios/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- https://animate.style/ -->
+  <div class="d-flex flex-column align-items-center">
+   <button class="btn btn-success" @click="getTodos()">Get Todos</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import axios from 'axios'
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    getTodos() {
+      axios.get('https://jsonplaceholder.typicode.com/todos/')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+  }
+});
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+</style>
